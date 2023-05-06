cd5ea532dcec2631cf06316e01643d2a279f5204
SanjuRwt <sanjusoftdev@gmail.com>
2023-05-06 18:38:45 +0530

    Assignement 1

---
+++ assigement 1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class St
+{
+    public static void main(String arr[])
+    {
+        Scanner sr=new Scanner(System.in);
+        String nm;
+        System.out.print("Enter Your Name:");
+        nm=sr.nextLine();
+        System.out.print(nm);
+    }
+}
