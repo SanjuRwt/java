9d909a623eb817aaa8672e44bf7ed70275903635
SanjuRwt <sanjusoftdev@gmail.com>
2023-05-22 10:05:58 +0530

    string

---
+++ string equal.java
@@ -0,0 +1,12 @@
+class Eqaul
+{
+    public static void main(String arr[])
+    {
+        String s1="sanju";
+        String s2="ajmer";
+        String s3="sanju";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+    }
+}
